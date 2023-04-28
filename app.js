3d7e5e99f902b76013c850af51738d5e2be15410
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-04-28 11:31:45 -0700

    edited gitignore and used `const` where applicable

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
-let $mainCont = $("<div id='mainCont' class='container text-center'></div>");
+const $mainCont = $("<div id='mainCont' class='container text-center'></div>");
 $("body").append($mainCont);
 
-let $topHeader = $("<header id='topHeader' class='py-3 mb-3 border-bottom'></header>");
+const $topHeader = $("<header id='topHeader' class='py-3 mb-3 border-bottom'></header>");
 $mainCont.append($topHeader);
 
-let $topHeaderH2 = $("<h2 class='py-2 border' style='background-color: var(--bs-warning-border-subtle); --bs-border-color: var(--bs-warning-border-subtle); color: var(--bs-warning-text);'>Beverage Concierge</h2>");
+const $topHeaderH2 = $("<h2 class='py-2 border' style='background-color: var(--bs-warning-border-subtle); --bs-border-color: var(--bs-warning-border-subtle); color: var(--bs-warning-text);'>Beverage Concierge</h2>");
 $topHeader.append($topHeaderH2);
 
-let $topHeaderCont = $("<div id='topHeaderCont' class='container-fluid d-grid gap-3 align-items-center' style='grid-template-columns: 1fr 2fr;'></div>");
+const $topHeaderCont = $("<div id='topHeaderCont' class='container-fluid d-grid gap-3 align-items-center' style='grid-template-columns: 1fr 2fr;'></div>");
 $topHeader.append($topHeaderCont);
 
-let $leftHeaderCol = $("<img src='bartender.jpg' width='100px' class='mx-auto d-block'></img>");
+const $leftHeaderCol = $("<img src='bartender.jpg' width='100px' class='mx-auto d-block'></img>");
 $topHeaderCont.append($leftHeaderCol);
 
-let $rightHeaderCol = $("<div id='rightHeaderCol' class='d-grid gap-2 d-md-flex justify-content-md-end'></div>");
+const $rightHeaderCol = $("<div id='rightHeaderCol' class='d-grid gap-2 d-md-flex justify-content-md-end'></div>");
 $topHeaderCont.append($rightHeaderCol);
 
-let $form = $("<form class='w-100 me-3' role='search'>");
+const $form = $("<form class='w-100 me-3' role='search'>");
 $rightHeaderCol.append($form);
 
-let $searchField = $("<input id='searchField' type='search'class='form-control' placeholder='Search...' aria-label='Search'>");
+const $searchField = $("<input id='searchField' type='search'class='form-control' placeholder='Search...' aria-label='Search'>");
 $form.append($searchField);
 
-let $submitBtn = $("<button id='submit' class='btn btn-primary' type='button'>Search</button>");
+const $submitBtn = $("<button id='submit' class='btn btn-primary' type='button'>Search</button>");
 $rightHeaderCol.append($submitBtn);
 
-let $resetBtn = $("<button id='reset' class='btn btn-primary btn-danger' type='button'>Reset</button>");
+const $resetBtn = $("<button id='reset' class='btn btn-primary btn-danger' type='button'>Reset</button>");
 $rightHeaderCol.append($resetBtn);
 
-let $results = $("<div id='results' class='container-fluid pb-2'></div>");
+const $results = $("<div id='results' class='container-fluid pb-2'></div>");
 $mainCont.append($results);
 
-let $resultsCont = $("<div id='resultsCont' class='d-grid gap-3' style='grid-template-columns: 1fr 2fr;'></div>");
+const $resultsCont = $("<div id='resultsCont' class='d-grid gap-3' style='grid-template-columns: 1fr 2fr;'></div>");
 $results.append($resultsCont);
 
-let $leftResultCol = $("<div id='leftResultCol' class='d-grid gap-2' style='align-content: start;'></div>");
+const $leftResultCol = $("<div id='leftResultCol' class='d-grid gap-2' style='align-content: start;'></div>");
 $resultsCont.append($leftResultCol);
 
-let $rightResultCol = $("<div id='rightResultCol' class='bg-light border rounded d-grid gap-2'></div>");
+const $rightResultCol = $("<div id='rightResultCol' class='bg-light border rounded d-grid gap-2'></div>");
 $resultsCont.append($rightResultCol);
 
-let $attribution = $("<p class='text-secondary' style='font-size: 10px;'>By Will Franceschini<br>A Galvanize bootcamp project<br>API credits: www.thecocktaildb.com/api.php<br>Logo credits: www.vecteezy.com</p>");
+const $attribution = $("<p class='text-secondary' style='font-size: 10px;'>By Will Franceschini<br>A Galvanize bootcamp project<br>API credits: www.thecocktaildb.com/api.php<br>Logo credits: www.vecteezy.com</p>");
 $mainCont.append($attribution);
 
-let gitHubDiv = document.createElement("div");
-let gitHubLogo = document.createElement("img");
+const gitHubDiv = document.createElement("div");
+const gitHubLogo = document.createElement("img");
 gitHubLogo.src = "./github-mark.png";
 gitHubLogo.style.display = "block";
 gitHubLogo.style.width = "2rem";
@@ -59,8 +59,8 @@ gitHubLogo.addEventListener("mouseover", () => {
         gitHubLogo.style.cursor = "pointer";
     });
 
-let linkedInDiv = document.createElement("div");
-let linkedInLogo = document.createElement("img");
+const linkedInDiv = document.createElement("div");
+const linkedInLogo = document.createElement("img");
 linkedInLogo.src = "https://img.shields.io/badge/-LinkedIn-0A66C2?style=flat&logo=Linkedin&logoColor=white";
 linkedInLogo.style.display = "block";
 linkedInLogo.style.margin = "0.5rem auto";
@@ -85,7 +85,7 @@ function initResults() {
 
 $(document).ready(() => {
     function searchAPI() {
-        let searchStr = $searchField.val();
+        const searchStr = $searchField.val();
         if (searchStr.length === 0) {
             initResults();
             $results.append("<div id='noSearchStr' class='bg-light border rounded'>Ops! Forgot to enter your search</div>");
